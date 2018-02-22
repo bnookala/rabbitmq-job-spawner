0e665eb359dc282adfb4e6b4660bf6936270b198
Bhargav Nookala <nooknb@gmail.com>
2018-02-22 12:53:09 -0800

    Adding helm chart to deploy

--- src/listener.py
+++ src/listener.py
@@ -23,6 +23,26 @@ class Listener(object):
                 pykube.KubeConfig.from_file(os.environ['KUBECONFIG'])
             )
 
+        if not os.environ['RABBIT_PASSWORD']:
+            print("RABBIT_PASSWORD not set in environment")
+            return
+
+        if not os.environ['RABBIT_URL']:
+            print("RABBIT_URL not set in environment")
+            return
+
+        if not os.environ['PORT']:
+            print("PORT not set in environment")
+            return
+
+        if not os.environ['QUEUE']:
+            print("QUEUE not set in environment")
+            return
+
+        if not os.environ['WORKLOAD']:
+            print("WORKLOAD not set in environment")
+            return
+
         credentials = pika.PlainCredentials(
             'user',
             os.environ['RABBIT_PASSWORD']
@@ -38,7 +58,9 @@ class Listener(object):
         self.connection = pika.BlockingConnection(parameters)
 
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=os.environ['QUEUE'])
+        self.channel.queue_declare(
+            queue=os.environ['QUEUE']
+        )
 
     # Spawns a job based on the queue.
     def callback(self, ch, method, properties, body):
@@ -46,10 +68,11 @@ class Listener(object):
             payload = str(body, 'utf-8')
             message_obj = json.loads(payload)
         except Exception:
-            print("Queue object could not be loaded: ")
+            print("Queued object could not be loaded: ")
             print(body)
             return
 
+        # example… workload (fix this later)
         encoding_type = message_obj.get('encoding_type')
         file_name = message_obj.get('file_name')
         file_loc = message_obj.get('file_loc')
@@ -60,7 +83,9 @@ class Listener(object):
         self.channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def create_job_with_message(self, file_name, file_loc, encoding_type):
-        spec_file = os.getcwd() + '/src/noop.json'
+        # todo: set the arguments as environment variables on the workload…
+
+        spec_file = os.getcwd() + '/workloads/' + os.environ.get('WORKLOAD')
 
         with open(spec_file, 'r') as spec:
             lines = spec.read()
